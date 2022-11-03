7e5e8d1334f90d9ac3ea6c414a525ea8b116c51a
Shinoo <s6203051623081@email.kmutnb.ac.th>
2022-11-03 22:38:45 +0700

    cart debug

--- pages/cart/index.js
+++ pages/cart/index.js
@@ -11,8 +11,7 @@ import { CgClose } from 'react-icons/cg'
 
 export default function Cart(){
     const [ list, setList ] = useState([])
-    const [ showDropdown, setShowDropdown ] = useState(false)
-    const [ selectAmount, setSelectAmount ] = useState(1)
+    const [ cart, steCart ] = useState([])
     const amount = [1,2,3,4,5,6,7,8,9,10]
 
     const getCart = () => {
@@ -25,7 +24,8 @@ export default function Cart(){
         })
         .then( result => {
             console.log(result.data)
-            setList(result.data)
+            setList(result.data.product)
+            steCart(result.data.userCartData)
         }).catch( err => {
             console.log(err)
         })
@@ -33,7 +33,15 @@ export default function Cart(){
 
     useEffect(()=> {
         getCart()
+        console.log('Is list & cart a array?')
+        console.log(Array.isArray(list))
+        console.log(Array.isArray(cart))
     },[])
+
+    useEffect(() => {
+        console.log(list)
+        console.log(cart)
+    },[list])
 
     return (
         <div>
@@ -46,7 +54,8 @@ export default function Cart(){
             <div className={styles.container}>
                 <main className={styles.main}>
                     <div className={styles.cartSection}>
-                        <div>รายการสินค้า</div>
+                        <div className={styles.label}>รายการสินค้า</div>
+                        <hr />
                         <div className={styles.list}>
                             {
                                 list.map( (element, index) => {
@@ -58,20 +67,25 @@ export default function Cart(){
                                             ee.classList.remove(styles.showDropdown)
                                         }
                                     }
+                                    const amountHandle = () => {
+
+                                    }
 
                                     return (
                                         <div key={`cart-item-${index}`} className={styles.item}>
                                             <div className={styles.image}>
-                                                {/* <Image src={element.img[0]} alt='img' layout='fill' objectFit='contain' /> */}
+                                                <Image src={element.img[0]} alt='img' layout='fill' objectFit='contain' />
                                             </div>
-                                            <div className={styles.mainContain}>
-                                                <div className={styles.title}>{element.title} {element.bookNum}</div>
+                                            <div className={styles.details}>
+                                                <Link href={`/series/${element.seriesId}/${element.url}`}>
+                                                    <a><div className={styles.title}>{element.title} {element.bookNum}</div></a>
+                                                </Link>
                                                 <div className={styles.category}>{element.category}</div>
                                                 <div className={styles.price}>{element.price} บาท</div>
                                             </div>
                                             <div className={styles.right}>
                                                 <div id={`cart-item-dropdown-${index}`} className={`${styles.dropdownGroup}`}>
-                                                    <div className={styles.dropdownSelection} onClick={e => showDropdownHandle(e)}><div id={`select-amount-${index}`}>{selectAmount}</div> <RiArrowDownSLine/></div>
+                                                    <div className={styles.dropdownSelection} onClick={e => showDropdownHandle(e)}><div id={`select-amount-${index}`}>{cart[index].amount}</div> <RiArrowDownSLine/></div>
                                                     <div className={styles.dropdownList} onClick={e => amountHandle(e)}>
                                                         { amount.map((element, index) => <div key={`amount-${index}`} className={styles.dropdownItem} data-value={element}>{element}</div> ) }
                                                     </div>
